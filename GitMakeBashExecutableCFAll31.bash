b9f4a8d1ff917f6dba67d204137779b071bf6e62
Mark Sobkow <mark.sobkow@gmail.com>
2026-02-27 06:33:05 -0600

    Final version

--- GitMakeBashExecutableCFAll31.bash
+++ GitMakeBashExecutableCFAll31.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
-for adir in $MSSCFHOME/v3_1-maven/java/* $MSSCFHOME/v3_1/java/*
-do
-		pushd $adir
-		chmod 755 `find . -name '*.bash'`
-		git add `find . -name '.gitignore' -o -name '*.bash'`
-		git update-index --add --chmod=+x `find . -name '*.bash'`
-		popd
-done
+if [ "$MCF_HOME" != "" ]; then
+	if [ -d "$MCF_HOME" ]; then
+		cd "$MCF_HOME"
+			for adir in *
+			do
+				pushd $adir
+				chmod 755 `find . -name '*.bash'`
+				git add `find . -name '.gitignore' -o -name '*.bash'`
+				git update-index --add --chmod=+x `find . -name '*.bash'`
+				popd
+			done
+		/usr/bin/ls -l --block-size=M
+	fi
+fi
