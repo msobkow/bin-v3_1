61bc4980ab4c2d80da24a39455565f856d97cdf0
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-08 12:51:02 -0600

    Shifting back to server.markhome.mcf from io.github.msobkow

--- BuildCFAll31.bash
+++ BuildCFAll31.bash
@@ -5,47 +5,47 @@ if [ "$CLIARGS" == "" ]; then
 fi
 pushd $MCF_HOME
 let MavenStatus=0
-#	io.github.msobkow.v3_1.cfsec.cfsecsaxramldr \
-#	io.github.msobkow.v3_1.cfint.cfintsaxramldr \
-#	io.github.msobkow.v3_1.cfbam.cfbamsaxramldr
+#	server.markhome.mcf.v3_1.cfsec.cfsecsaxramldr \
+#	server.markhome.mcf.v3_1.cfint.cfintsaxramldr \
+#	server.markhome.mcf.v3_1.cfbam.cfbamsaxramldr
 for aprojdir in \
-	io.github.msobkow \
-	io.github.msobkow.v3_1 \
+	server.markhome.mcf \
+	server.markhome.mcf.v3_1 \
 	\
-	io.github.msobkow.v3_1.cflib \
-	io.github.msobkow.v3_1.cflib.dbutil \
-	io.github.msobkow.v3_1.cflib.dbtest \
-	io.github.msobkow.v3_1.cflib.javafx \
+	server.markhome.mcf.v3_1.cflib \
+	server.markhome.mcf.v3_1.cflib.dbutil \
+	server.markhome.mcf.v3_1.cflib.dbtest \
+	server.markhome.mcf.v3_1.cflib.javafx \
 	\
-	io.github.msobkow.v3_1.cfsec.cfsec \
-	io.github.msobkow.v3_1.cfsec.cfsecbuff \
-	io.github.msobkow.v3_1.cfsec.cfsecjpa \
-	io.github.msobkow.v3_1.cfsec.cfsecjpatest \
-	io.github.msobkow.v3_1.cfsec.cfsecsaxloader \
-	io.github.msobkow.v3_1.cfsec.cfsecjavafx \
-	io.github.msobkow.v3_1.cfsec.cfseccustfx \
-	io.github.msobkow.v3_1.cfsec.cfsecram \
-	io.github.msobkow.v3_1.cfsec.cfsecramtest \
+	server.markhome.mcf.v3_1.cfsec.cfsec \
+	server.markhome.mcf.v3_1.cfsec.cfsecbuff \
+	server.markhome.mcf.v3_1.cfsec.cfsecjpa \
+	server.markhome.mcf.v3_1.cfsec.cfsecjpatest \
+	server.markhome.mcf.v3_1.cfsec.cfsecsaxloader \
+	server.markhome.mcf.v3_1.cfsec.cfsecjavafx \
+	server.markhome.mcf.v3_1.cfsec.cfseccustfx \
+	server.markhome.mcf.v3_1.cfsec.cfsecram \
+	server.markhome.mcf.v3_1.cfsec.cfsecramtest \
 	\
-	io.github.msobkow.v3_1.cfint.cfint \
-	io.github.msobkow.v3_1.cfint.cfintbuff \
-	io.github.msobkow.v3_1.cfint.cfintjpa \
-	io.github.msobkow.v3_1.cfint.cfintjpatest \
-	io.github.msobkow.v3_1.cfint.cfintsaxloader \
-	io.github.msobkow.v3_1.cfint.cfintjavafx \
-	io.github.msobkow.v3_1.cfint.cfintcustfx \
-	io.github.msobkow.v3_1.cfint.cfintram \
-	io.github.msobkow.v3_1.cfint.cfintramtest \
+	server.markhome.mcf.v3_1.cfint.cfint \
+	server.markhome.mcf.v3_1.cfint.cfintbuff \
+	server.markhome.mcf.v3_1.cfint.cfintjpa \
+	server.markhome.mcf.v3_1.cfint.cfintjpatest \
+	server.markhome.mcf.v3_1.cfint.cfintsaxloader \
+	server.markhome.mcf.v3_1.cfint.cfintjavafx \
+	server.markhome.mcf.v3_1.cfint.cfintcustfx \
+	server.markhome.mcf.v3_1.cfint.cfintram \
+	server.markhome.mcf.v3_1.cfint.cfintramtest \
 	\
-	io.github.msobkow.v3_1.cfbam.cfbam \
-	io.github.msobkow.v3_1.cfbam.cfbambuff \
-	io.github.msobkow.v3_1.cfbam.cfbamjpa \
-	io.github.msobkow.v3_1.cfbam.cfbamjpatest \
-	io.github.msobkow.v3_1.cfbam.cfbamsaxloader \
-	io.github.msobkow.v3_1.cfbam.cfbamjavafx \
-	io.github.msobkow.v3_1.cfbam.cfbamcustfx \
-	io.github.msobkow.v3_1.cfbam.cfbamram \
-	io.github.msobkow.v3_1.cfbam.cfbamramtest
+	server.markhome.mcf.v3_1.cfbam.cfbam \
+	server.markhome.mcf.v3_1.cfbam.cfbambuff \
+	server.markhome.mcf.v3_1.cfbam.cfbamjpa \
+	server.markhome.mcf.v3_1.cfbam.cfbamjpatest \
+	server.markhome.mcf.v3_1.cfbam.cfbamsaxloader \
+	server.markhome.mcf.v3_1.cfbam.cfbamjavafx \
+	server.markhome.mcf.v3_1.cfbam.cfbamcustfx \
+	server.markhome.mcf.v3_1.cfbam.cfbamram \
+	server.markhome.mcf.v3_1.cfbam.cfbamramtest
 do
 	if [ "$MavenStatus" == "0" ]; then
 		if [ -a ${aprojdir}/pom.xml ]; then
@@ -60,41 +60,41 @@ do
 	fi
 done
 if [ "$MavenStatus" == "0" ]; then
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cflib/3.1/io.github.msobkow.v3_1.cflib-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cflib.dbutil/3.1/io.github.msobkow.v3_1.cflib.dbutil-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cflib.dbtest/3.1/io.github.msobkow.v3_1.cflib.dbtest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cflib.javafx/3.1/io.github.msobkow.v3_1.cflib.javafx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsec/3.1/io.github.msobkow.v3_1.cfsec.cfsec-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecbuff/3.1/io.github.msobkow.v3_1.cfsec.cfsecbuff-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecjpa/3.1/io.github.msobkow.v3_1.cfsec.cfsecjpa-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecjpatest/3.1/io.github.msobkow.v3_1.cfsec.cfsecjpatest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecsaxloader/3.1/io.github.msobkow.v3_1.cfsec.cfsecsaxloader-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecram/3.1/io.github.msobkow.v3_1.cfsec.cfsecram-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecramtest/3.1/io.github.msobkow.v3_1.cfsec.cfsecramtest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecsaxramldr/3.1/io.github.msobkow.v3_1.cfsec.cfsecsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfsecjavafx/3.1/io.github.msobkow.v3_1.cfsec.cfsecjavafx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfsec.cfseccustfx/3.1/io.github.msobkow.v3_1.cfsec.cfseccustfx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfint/3.1/io.github.msobkow.v3_1.cfint.cfint-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintbuff/3.1/io.github.msobkow.v3_1.cfint.cfintbuff-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintjpa/3.1/io.github.msobkow.v3_1.cfint.cfintjpa-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintjpatest/3.1/io.github.msobkow.v3_1.cfint.cfintjpatest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintsaxloader/3.1/io.github.msobkow.v3_1.cfint.cfintsaxloader-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintram/3.1/io.github.msobkow.v3_1.cfint.cfintram-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintramtest/3.1/io.github.msobkow.v3_1.cfint.cfintramtest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintsaxramldr/3.1/io.github.msobkow.v3_1.cfint.cfintsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintjavafx/3.1/io.github.msobkow.v3_1.cfint.cfintjavafx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfint.cfintcustfx/3.1/io.github.msobkow.v3_1.cfint.cfintcustfx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbam/3.1/io.github.msobkow.v3_1.cfbam.cfbam-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbambuff/3.1/io.github.msobkow.v3_1.cfbam.cfbambuff-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamjpa/3.1/io.github.msobkow.v3_1.cfbam.cfbamjpa-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamjpatest/3.1/io.github.msobkow.v3_1.cfbam.cfbamjpatest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamsaxloader/3.1/io.github.msobkow.v3_1.cfbam.cfbamsaxloader-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamram/3.1/io.github.msobkow.v3_1.cfbam.cfbamram-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamramtest/3.1/io.github.msobkow.v3_1.cfbam.cfbamramtest-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamsaxramldr/3.1/io.github.msobkow.v3_1.cfbam.cfbamsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamjavafx/3.1/io.github.msobkow.v3_1.cfbam.cfbamjavafx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $HOME/.m2/repository/io/github/msobkow/v3_1/io.github.msobkow.v3_1.cfbam.cfbamcustfx/3.1/io.github.msobkow.v3_1.cfbam.cfbamcustfx-3.1.jar $MCF_HOME/bin-v3_1
-	cp -v $MCF_HOME/io.github.msobkow.v3_1.*/target/*-spring-boot.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cflib/3.1/server.markhome.mcf.v3_1.cflib-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cflib.dbutil/3.1/server.markhome.mcf.v3_1.cflib.dbutil-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cflib.dbtest/3.1/server.markhome.mcf.v3_1.cflib.dbtest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cflib.javafx/3.1/server.markhome.mcf.v3_1.cflib.javafx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsec/3.1/server.markhome.mcf.v3_1.cfsec.cfsec-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecbuff/3.1/server.markhome.mcf.v3_1.cfsec.cfsecbuff-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecjpa/3.1/server.markhome.mcf.v3_1.cfsec.cfsecjpa-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecjpatest/3.1/server.markhome.mcf.v3_1.cfsec.cfsecjpatest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecsaxloader/3.1/server.markhome.mcf.v3_1.cfsec.cfsecsaxloader-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecram/3.1/server.markhome.mcf.v3_1.cfsec.cfsecram-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecramtest/3.1/server.markhome.mcf.v3_1.cfsec.cfsecramtest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecsaxramldr/3.1/server.markhome.mcf.v3_1.cfsec.cfsecsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfsecjavafx/3.1/server.markhome.mcf.v3_1.cfsec.cfsecjavafx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfsec.cfseccustfx/3.1/server.markhome.mcf.v3_1.cfsec.cfseccustfx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfint/3.1/server.markhome.mcf.v3_1.cfint.cfint-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintbuff/3.1/server.markhome.mcf.v3_1.cfint.cfintbuff-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintjpa/3.1/server.markhome.mcf.v3_1.cfint.cfintjpa-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintjpatest/3.1/server.markhome.mcf.v3_1.cfint.cfintjpatest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintsaxloader/3.1/server.markhome.mcf.v3_1.cfint.cfintsaxloader-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintram/3.1/server.markhome.mcf.v3_1.cfint.cfintram-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintramtest/3.1/server.markhome.mcf.v3_1.cfint.cfintramtest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintsaxramldr/3.1/server.markhome.mcf.v3_1.cfint.cfintsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintjavafx/3.1/server.markhome.mcf.v3_1.cfint.cfintjavafx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfint.cfintcustfx/3.1/server.markhome.mcf.v3_1.cfint.cfintcustfx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbam/3.1/server.markhome.mcf.v3_1.cfbam.cfbam-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbambuff/3.1/server.markhome.mcf.v3_1.cfbam.cfbambuff-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamjpa/3.1/server.markhome.mcf.v3_1.cfbam.cfbamjpa-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamjpatest/3.1/server.markhome.mcf.v3_1.cfbam.cfbamjpatest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamsaxloader/3.1/server.markhome.mcf.v3_1.cfbam.cfbamsaxloader-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamram/3.1/server.markhome.mcf.v3_1.cfbam.cfbamram-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamramtest/3.1/server.markhome.mcf.v3_1.cfbam.cfbamramtest-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamsaxramldr/3.1/server.markhome.mcf.v3_1.cfbam.cfbamsaxramldr-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamjavafx/3.1/server.markhome.mcf.v3_1.cfbam.cfbamjavafx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $HOME/.m2/repository/server/markhome/mcf/v3_1/server.markhome.mcf.v3_1.cfbam.cfbamcustfx/3.1/server.markhome.mcf.v3_1.cfbam.cfbamcustfx-3.1.jar $MCF_HOME/bin-v3_1
+	cp -v $MCF_HOME/server.markhome.mcf.v3_1.*/target/*-spring-boot.jar $MCF_HOME/bin-v3_1
 	echo "SUCCESS: All 3.1 Java builds completed successfully"
 fi
 popd
